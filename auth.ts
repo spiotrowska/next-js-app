0636115c6ddbda6f5f3b998a9f1e3ef28fbb3fc8
sylwia.wochniak <sylwia.wochniak@home.eu>
2025-10-16 12:55:26 +0200

    fix user auth

--- auth.ts
+++ auth.ts
@@ -2,35 +2,37 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
-// import { writeClient } from "@/sanity/lib/write-client";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    // async signIn({
-    //   user: { name, email, image },
-    //   profile: { id, login, bio },
-    // }) {
-    //   const existingUser = await client
-    //     .withConfig({ useCdn: false })
-    //     .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-    //       id,
-    //     });
-    //
-    //   if (!existingUser) {
-    //     await writeClient.create({
-    //       _type: "author",
-    //       id,
-    //       name,
-    //       username: login,
-    //       email,
-    //       image,
-    //       bio: bio || "",
-    //     });
-    //   }
-    //
-    //   return true;
-    // },
+    async signIn({ user: { name, email, image }, profile }) {
+      if (!profile || typeof profile !== "object" || !("id" in profile) || !("login" in profile)) {
+        return false;
+      }
+      const { id, login, bio } = profile as { id: string; login: string; bio?: string };
+
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+    
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+    
+      return true;
+    },
     async jwt({ token, account, profile }) {
       if (account && profile) {
         const user = await client
